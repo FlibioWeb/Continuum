c470bcef1a49f52355c0e2601184b39f3f8f2faa
Flibio <flibio.business@gmail.com>
2016-08-30 07:54:16 -0600

    Add commit SHA and link to changes on project build page

--- partials/projectbuild.php
+++ partials/projectbuild.php
@@ -15,6 +15,11 @@
         $log = "<i class=\"fa fa-list-alt\"></i><a href=\"https://travis-ci.org/".$data["github"]."/builds/".$buildData["job"]."\">Build Log</a>";
     }
 
+    $diff = false;
+    if(isset($buildData["commit"], $data["github"])) {
+        $diff = "<i class=\"fa fa-file-text\"></i><a href=\"https://github.com/".$data["github"]."/commit/".$buildData["commit"]."\">File Changes</a>";
+    }
+
     $artifacts = "";
 
     if($build > 0) {
@@ -27,12 +32,16 @@
     <i class="fa fa-arrow-left"></i><a href="<?php echo BASEPATH."project/".$project; ?>">Back to <?php echo $data["display"]; ?></a>
     <p>
     <?php if($log !== false) echo $log; ?>
+    <p>
+    <?php if($diff !== false) echo $diff; ?>
 </div>
 <div class="projectInfo">
     <span class="title"><?php echo $data["display"]." Build #".$build; ?></span>
     <p>
     <code><?php echo nl2br($message); ?></code>
     </p>
+    <b class="semibold">Commit SHA: </b><code><?php echo $buildData["commit"]; ?></code>
+    <p>
     <span class="subtitle">Build Artifacts</span>
     <p>
     <?php echo $artifacts; ?>
